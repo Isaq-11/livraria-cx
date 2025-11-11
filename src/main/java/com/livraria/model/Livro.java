9f7302714cce4566f7d88ea83355333c062590ab
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2025-11-11 17:00:12 -0300

    modified some attributes

--- src/main/java/com/livraria/model/Livro.java
+++ src/main/java/com/livraria/model/Livro.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Entity
 @Table(name="livro")
 public class Livro {
- classificalçao indicaiva
+ classificalçao indicaivafd
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
