741ce28a7bbb87da1ae91f9b2604088cc28ca4be
Isaque <isaquecarvalhoxavier@gmail.com>
2025-11-12 14:14:14 -0300

    modifying Livro class

--- src/main/java/com/livraria/model/Fornecedor.java
+++ src/main/java/com/livraria/model/Fornecedor.java
@@ -7,7 +7,26 @@ import java.time.LocalDate;
 @DiscriminatorValue("fornecedor")
 public class Fornecedor extends PessoaJuridica{
 
-    private String representanteNome
+    @Column(name= "nome_representante", nullable = false)
+    private String representanteNome;
+
+    @Column(name= "inscricao_estadual", nullable = false)
+    private String inscricaoEstadual;
+
+    public Fornecedor(){}
+    public Fornecedor(String nomeFantasia, String razaoSocial, String cnpj, String inscricaoEstadual,
+                      String representanteNome, String telefoneResponsavel, String telefoneCorporativo,
+                      String emailCorporativo, String endereco, String cep){
+        super(nomeFantasia, razaoSocial, cnpj, telefoneResponsavel, telefoneCorporativo, emailCorporativo,
+                endereco, cep);
+        this.representanteNome= representanteNome;
+        this.inscricaoEstadual= inscricaoEstadual;}
+
+    public String getRepresentanteNome(){return  representanteNome;}
+    public void setRepresentanteNome(String representanteNome){this.representanteNome= representanteNome;}
+
+    public String getInscricaoEstadual(){return  inscricaoEstadual;}
+    public void setInscricaoEstadual(String inscricaoEstadual){this.inscricaoEstadual= inscricaoEstadual;}
 
     
 
--- src/main/java/com/livraria/model/Livro.java
+++ src/main/java/com/livraria/model/Livro.java
@@ -2,73 +2,22 @@ package com.livraria.model;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name="livro")
+@Table(name= "livro")
 public class Livro {
- classificalçao indicaivafd
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
-
-    @Column(nullable = false, length = 100)
     private String titulo;
-
-    @Column(nullable = false, length = 6)
-    private String ano;
-
-    @ManyToOne
-    @JoinColumn(name= "editora_id")
+    private int anoPublicacao;
+    private int classificacaoIndicativa;
+    private String isbn;
+    //autor
     private Editora editora;
-
-    //@Column(nullable = false, length = 6)
-    private Fornecedor fornecedor;
-
-    //@Column(nullable = false, length = 6)
-    private Autor autor;
-
-    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal preco;
+    private int qtdeEstoque; // seria bom implementar uma classe só pra isso, mas por enquanto nao
 
 
 
-    //@ManyToMany
-    //@JoinTable(name="autor_livro",
-    //        joinColumns = @JoinColumn (name= "id_livro"),
-    //        inverseJoinColumns = @JoinColumn(name="id_autor"))
-    //private List<Autor> autores = new ArrayList<>();
-
-    public Livro(){}
-    public Livro(String titulo, String ano, BigDecimal preco){
-        this.titulo=titulo;
-        this.ano = ano;
-        this.preco = preco;
-    }
-
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id=id;};
-
-    public String getTitulo() {return titulo;}
-    public void setTitulo(String titulo) {this.titulo=titulo;}
-
-    //public List<Autor> getAutores() { return autores; }
-    //public void setAutores(List<Autor> autores) { this.autores = autores; }
-
-    //// Métodos para manter o relacionamento bidirecional
-    //public void addAutor(Autor autor) {
-    //    if (!autores.contains(autor)) {
-    //        autores.add(autor);
-    //        autor.getLivros().add(this); // mantém consistência
-    //    }
-    //}
-//
-    //public void removeAutor(Autor autor) {
-    //    if (autores.contains(autor)) {
-    //        autores.remove(autor);
-    //        autor.getLivros().remove(this); // mantém consistência
-    //    }
-    //}
 
 }
