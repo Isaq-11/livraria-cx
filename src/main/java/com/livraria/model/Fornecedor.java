d1d86449fe3a35c7322a131e557d14501ca0f596
Isaque <isaquecarvalhoxavier@gmail.com>
2025-11-12 11:10:52 -0300

    modifying fornecedor and editora classes

--- src/main/java/com/livraria/model/Fornecedor.java
+++ src/main/java/com/livraria/model/Fornecedor.java
@@ -2,17 +2,7 @@ package com.livraria.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name= "fornecedor")
-public class Fornecedor extends Cnpj{
+@DiscriminatorValue("fornecedor")
+public class Fornecedor extends PessoaJuridica{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    public Fornecedor(){}
-    public Fornecedor(String nome, String razaoSocial, String cnpj, String email, String telefone){
-        super(nome, cnpj, razaoSocial, email, telefone);
-
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id=id;}
 }
