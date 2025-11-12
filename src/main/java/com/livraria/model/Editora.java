cbba9a7f7a1defd35ba62f5fe1f9b9f87072b21a
Isaque <isaquecarvalhoxavier@gmail.com>
2025-11-12 13:45:45 -0300

    modifying fornecedor and editora classes

--- src/main/java/com/livraria/model/Editora.java
+++ src/main/java/com/livraria/model/Editora.java
@@ -1,4 +1,35 @@
 package com.livraria.model;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
-public class Editora {
+@Entity
+@DiscriminatorValue("editora")
+public class Editora extends PessoaJuridica{
+
+    @Column(name= "ano_fundacao", nullable = false)
+    private int anoFundacao;
+
+    @Column(name= "editor_chefe", nullable = false)
+    private String editorChefe;
+
+    @Column(name= "qtde_titulos_publicados")
+    private int titulosPublicados= 0; // nao passei no construtor pq quero que seja incrementado automaticamente
+
+    public Editora(){}
+    public Editora(String nomeFantasia, String razaoSocial, String cnpj, String telefoneResponsavel,
+                   String telefoneCorporativo, String emailCorporativo, String endereco, String cep,
+                   int  anoFundacao, String editorChefe){
+        super(nomeFantasia, razaoSocial, cnpj, telefoneResponsavel, telefoneCorporativo,
+                emailCorporativo, endereco, cep);
+        this.anoFundacao= anoFundacao;
+        this.editorChefe= editorChefe;
+    }
+
+    public int getAnoFundacao(){return anoFundacao;}
+    public void setAnoFundacao(int anoFundacao){this.anoFundacao= anoFundacao;}
+
+    public String getEditorChefe(){return editorChefe;}
+    public void setEditorChefe(String editorChefe){this.editorChefe= editorChefe;}
+
+    public int getTitulosPublicados(){return titulosPublicados;}
 }
--- src/main/java/com/livraria/model/Fornecedor.java
+++ src/main/java/com/livraria/model/Fornecedor.java
@@ -1,8 +1,14 @@
 package com.livraria.model;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @DiscriminatorValue("fornecedor")
 public class Fornecedor extends PessoaJuridica{
+
+    private String representanteNome
+
+    
 
 }
