9b9f1370dd8cb34d806b4e6d5d1d2a14caec435f
Isaque <isaquecarvalhoxavier@gmail.com>
2025-11-13 16:52:36 -0300

    feat(dao): adiciona emf e em, alem do construtor
    fazer uma classe para jpautil e colocar o emf lá, pois ele precisa ser aberto e fechado apenas uma vez durante a execução do preojeto

--- src/main/java/com/livraria/dao/DAOGenericoAbstrato.java
+++ src/main/java/com/livraria/dao/DAOGenericoAbstrato.java
@@ -1,7 +1,26 @@
 package com.livraria.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
 
 public abstract class DAOGenericoAbstrato<T, ID extends Serializable> implements
         InterfaceDAOGenerica<T, ID>{
+
+    private static final EntityManagerFactory emf=
+            Persistence.createEntityManagerFactory("livrariaPU");
+
+    protected EntityManager em= emf.createEntityManager();
+    private Class<T> entityClass;
+
+    @SuppressWarnings("unchecked")
+    public DAOGenericoAbstrato(){
+        this.entityClass= (Class<T>)
+                ((ParameterizedType) getClass()
+                .getGenericSuperclass())
+                .getActualTypeArguments()[0];
+    }
+
 }
